7d79dfc61206aff1db275834387288fd02ca063c
Muhammad hassan sabri <hassan.life@gmail.com>
2025-08-23 01:50:52 +0500

    Update ultra_marketing.sql

--- ultra_marketing.sql
+++ ultra_marketing.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
+-- 
 -- Host: 127.0.0.1
 -- Generation Time: Aug 22, 2025 at 10:42 PM
 -- Server version: 10.4.28-MariaDB
